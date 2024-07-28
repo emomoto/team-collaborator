e7208a83fe4dc4ea86abc1a5998632612ad7f7cf
emomoto <163348701+emomoto@users.noreply.github.com>
2024-07-28 08:23:06 +0200

    optimization

--- server/models.py
+++ server/models.py
@@ -28,7 +28,7 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return f'<User {self.username}>'
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -39,7 +39,7 @@ class Task(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
-        return '<Task {}>'.format(self.title)
+        return f'<Task {self.title}>'
 
 class Message(db.Model):
     __tablename__ = 'messages'
@@ -48,19 +48,19 @@ class Message(db.Model):
     sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     sender = db.relationship('User', foreign_keys=[sender_id], backref='sent_messages')
-    receiver = db.relationship('User', foreign.validators=[receiver_id], backref='received_messages')
+    receiver = db.relationship('User', foreign_keys=[receiver_id], backref='received_messages')
 
     def __repr__(self):
-        return '<Message from {} to {}>'.format(self.sender_id, self.receiver_id)
+        return f'<Message from {self.sender_id} to {self.receiver_id}>'
 
 @app.route('/api/users', methods=['POST'])
 def create_user():
     username = request.json.get('username')
     email = request.json.get('email')
     password = request.json.get('password')
-    if username is None or password is None:
+    if not username or not password:
         return jsonify({'message': 'Missing arguments'}), 400
-    if User.query.filter_by(username=username).first() is not none:
+    if User.query.filter_by(username=username).first():
         return jsonify({'message': 'User already exists'}), 400
     user = User(username=username, email=email)
     user.set_password(password)
@@ -68,25 +68,32 @@ def create_user():
     db.session.commit()
     return jsonify({'username': user.username}), 201, {'Location': f'/api/users/{user.id}'}
 
-@app.route('/api/tasks', methods=['POST'])
+@app.route('/api/bulk-tasks', methods=['POST'])
 @auth.login_required
-def create_task():
-    title = request.json.get('title')
-    description = request.json.get('description')
-    user_id = auth.current_user().id
-    if title is None:
-        return jsonify({'message': 'Missing title'}), 400
-    task = Task(title=title, description=description, user_id=user_id)
-    db.session.add(task)
+def create_bulk_tasks():
+    tasks = request.json.get('tasks')
+    if not tasks:
+        return jsonify({'message': 'Missing tasks'}), 400
+    created_tasks = []
+    for task_info in tasks:
+        title = task_info.get('title')
+        description = task_info.get('description')
+        if title is None:
+            continue  # Or handle the error as you see fit
+        user_id = auth.current_user().id
+        task = Task(title=title, description=description, user_id=user_id)
+        db.session.add(task)
+        created_tasks.append(task)
     db.session.commit()
-    return jsonify({'title': task.title}), 201, {'Location': f'/api/tasks/{task.id}'}
+    tasks_info = [{'id': task.id, 'title': task.title} for task in created_tasks]
+    return jsonify(tasks_info), 201
 
 @auth.verify_password
 def verify_password(username_or_token, password):
     user = User.query.filter_by(username=username_or_token).first()
     if not user or not user.check_password(password):
         return False
-    return True
+    return user  # Return the user to use it in `auth.current_user()`
 
 with app.app_context():
     db.create_all()
